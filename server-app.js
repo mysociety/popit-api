09af0ea07edb0ac24ae284edf75bb0cb13f5738c
Edmund von der Burg <evdb@mysociety.org>
2013-03-14 15:53:15 +0000

    Extract sever app bits out to different file so that it is easy to use in tests

---
+++ server-app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var express = require('express'),
+    apiApp  = require('./')
+
+var app = module.exports = express();
+
+// Make dev easier
+app.use(express.errorHandler());
+app.use(express.favicon());
+
+// Use vhost to route to the correct app
+app.use( '/api', apiApp );
+
+// Handle everything else as a 404
+app.use(function (req, res) {
+  res.send(
+    'page not found - try <a href="/api">/api</a>',
+    404
+  );
+});
--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 "use strict";
 
-var express = require('express'),
-    apiApp  = require('./')
+var serverApp  = require('./server-app')
 
-var app = module.exports = express();
+serverApp.use(express.logger('dev'));
 
-// Make dev easier
-app.use(express.logger('dev'));
-app.use(express.errorHandler());
-app.use(express.favicon());
-
-// Use vhost to route to the correct app
-app.use( '/api', apiApp );
-
-// Handle everything else as a 404
-app.use(function (req, res) {
-  res.send(
-    'page not found - try <a href="/api">/api</a>',
-    404
-  );
-});
-
-app.listen(3000);
+serverApp.listen(3000);
 console.log('listening on http://0.0.0.0:3000');
