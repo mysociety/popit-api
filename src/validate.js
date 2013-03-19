102507b4bfbad4c6fa7b3bd24db07bcaea06116a
Edmund von der Burg <evdb@mysociety.org>
2013-03-19 15:50:12 +0000

    Remove commented out experimental code

--- src/validate.js
+++ src/validate.js
@@ -19,10 +19,6 @@ _.each(schemas, function (schema) {
   }
 });
 
-// env.createSchema(schemas['http://popoloproject.com/schemas/person.json#'], null, 'http://popoloproject.com/schemas/person.json#');
-// env.createSchema(schemas['http://popoloproject.com/schemas/other_name#'], null, 'http://popoloproject.com/schemas/other_name.json#');
-
-
 
 module.exports = function (name, data, callback) {
   
@@ -40,55 +36,9 @@ module.exports = function (name, data, callback) {
   schema = schemas[schemaUrl];
   assert(schema, "Could not get schema for '" + schemaUrl + "'");
 
-  // var schema = schemas[schemaUrl];
   var report = env.validate(data, schema);
 
-  // console.log( report.errors);
-  
   callback(null, report.errors );
 
 };
 
-
-
-// I looked at using JaySchema but it just did not seem to be able to do the basic validation :(
-// I'm probably getting something wrong.
-//
-// var JaySchema     = require('jayschema');
-// 
-// var loader = function loader(ref, callback) {
-// 
-//   // Add the trailing hash if needed
-//   if (! /#/.test(ref)) {
-//     ref += "#";
-//   }
-// 
-//   var schema = schemas[ref];
-// 
-//   if (schema) {
-//     callback( null, schema );
-//   } else {
-//     callback("Could not load schema for '" + ref  + "'");
-//   }
-// };
-// 
-// 
-// 
-// module.exports = function (collectionName, data, callback) {
-//   
-//   var js = new JaySchema(loader);
-// 
-//   var collection = collections[collectionName];
-//   assert(collection, "Could not load collection for '" + collectionName + "'");
-//   var schemaUrl = collection.popoloSchemaUrl;
-//   assert(schemaUrl, "Could not get url from collection'" + collectionName + "'");
-// 
-//   js.validate(
-//     data,
-//     schemaUrl,
-//     function (errors) {
-//       callback(null, errors || [] );
-//     }
-//   );
-// 
-// };
