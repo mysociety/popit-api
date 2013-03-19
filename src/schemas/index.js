aa39ef3e7f2a9a35452706e504aba6213f41e5dd
Edmund von der Burg <evdb@mysociety.org>
2013-03-19 15:32:59 +0000

    Manually replace {$ref: ...} entries with the schema they refer to. Workaround for garycourt/JSV#68

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -26,6 +26,40 @@ _.each( allSchemas, function (schema) {
 
 
 
+// go through all the url schemas and make sure that any $refs are replaced with
+// the schema they refer to. This is a workaround for
+// https://github.com/garycourt/JSV/issues/68
+_.each( popoloSchemas, function (schema) {
+  swapOutDollarRefs(schema);
+});
+
+function swapOutDollarRefs (data) {
+  
+  // If we are not a dict or array don't continue
+  if (!_.isObject(data)) {
+    return;
+  }
+
+  // look at all the contents of data to see if it needs swapping
+  _.each(data, function (val, key) {
+    // Is there a $ref to replace?
+    if (_.isObject(val)) {
+      if (_.has(val, '$ref')) {
+        var $ref = val.$ref;
+        var refSchema = urlSchemas[$ref];
+        assert(refSchema, "Could not find schema for '" + $ref + "'");
+
+        // swap out the contents
+        data[key] = refSchema;
+      }
+    }
+  });
+
+
+  // recurse down
+  _.each(data, swapOutDollarRefs);
+    
+}
 
 
 module.exports = urlSchemas;
