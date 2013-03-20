526e57c0f979e7b09d20effb8a7902f23d883df8
Edmund von der Burg <evdb@mysociety.org>
2013-03-20 10:58:34 +0000

    Very basic storage abstraction

--- src/storage.js
+++ src/storage.js
@@ -1,6 +1,79 @@
+/* jshint camelcase: false */
+
 "use strict";
 
-var Storage = function () {};
+var mongo  = require('mongodb'),
+    assert = require('assert');
+ 
+var server = new mongo.Server('localhost', 27017, {auto_reconnect: true});
+
+// store the databases in here as they are created.
+var databases = {};
+
+
+var Storage = function (databaseName) {
+  assert(databaseName, "Need to provide a database name");
+  this.databaseName = databaseName;
+};
+
+/*
+  Get ready to do stuff. Connect to the database.
+*/
+Storage.prototype.init = function (cb) {
+  console.log('init');
+  var storage = this;
+
+  // db already set up, store it and move on
+  if (databases[this.databaseName]) {
+    storage.db = databases[storage.databaseName];
+    return cb(null);
+  }
+
+  // open new database
+  var db = new mongo.Db(storage.databaseName, server, {journal: true});
+  
+  db.open(function (err, db) {
+    if (!err) {
+      databases[storage.databaseName] = db;
+      storage.db = db;
+    }
+    cb(err);
+  });
+};
+
+/*
+  Empty the database of all data. Not something you'd want to do in production :)
+*/
+Storage.prototype.empty = function (cb) {
+  console.log('empty');
+  cb();
+};
+
+
+/*
+  Store a document in the database.
+*/
+Storage.prototype.store = function (collectionName, doc, cb) {
+  console.log('store');
+  var collection = this.db.collection(collectionName);
+  collection.update({_id: doc.id}, doc, {upsert: true}, function (err) {
+    cb(err, doc);
+  });
+};
+
+/*
+  Retrieve a document from the database.
+*/
+Storage.prototype.retrieve = function (collectionName, id, cb) {
+  console.log('retrieve');
+  var collection = this.db.collection(collectionName);
+  collection.findOne({_id: id}, function (err, doc) {
+    if (!err) {
+      doc.id = doc._id;
+      delete doc._id;
+    }
+    cb(err, doc);
+  });
+};
+
 module.exports = Storage;
-
-
--- test/storage.js
+++ test/storage.js
@@ -3,12 +3,41 @@
 var Storage = require("../src/storage"),
     assert  = require('assert');
 
-var storage = new Storage();
+
+var storage = new Storage('test-popit-db');
+
 
 describe("Storage", function () {
 
-  beforeEach(function (done) {
-    done();
+  before(function (done) {
+    storage.init(done);
+  });
+
+  describe("store and retrieve", function () {
+
+    before(function (done) {
+      storage.empty(done);
+    });
+
+    var sampleData = { id: 'test', foo: 'bar' };
+
+    it("store some data", function (done) {
+      storage.store('samples', sampleData, function (err, doc) {
+        assert.ifError(err);
+        assert.deepEqual(doc, sampleData);
+        done();
+      });
+    });
+
+    it("retrieve it", function (done) {
+      storage.retrieve('samples', sampleData.id, function (err, doc) {
+        assert.ifError(err);
+        assert(doc);
+        assert.deepEqual( doc, sampleData );
+        done();
+      });
+    });
+    
   });
   
   describe("empty", function () {
