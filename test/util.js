dff2083ad478c958e9588b04048f0901724f8d97
Chris Mytton <self@hecticjeff.net>
2014-01-06 17:23:03 +0000

    Add loadFixture test helper using mongoimport
    Shelling out to mongoimport(1) is much faster than trying to do a bulk
    insert from node. This can be used in tests to load up json files
    produced by mongoexport(1).

--- test/util.js
+++ test/util.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var defaults = require('./defaults');
+
 exports.person = person;
 
 function person(attrs) {
@@ -9,4 +13,38 @@ function person(attrs) {
   attrs.identifiers = [];
   attrs.other_names = [];
   return attrs;
+}
+
+exports.loadFixture = loadFixture;
+
+/**
+ * Populate collection using the json at fixturePath. The json file
+ * should be produced by mongoexport.
+ *
+ * @param {String} collection The name of the collection to populate
+ * @param {String} fixturePath Path to a json file produced by mongoexport
+ * @return {Function} A function that can be used in mocha hooks
+ */
+function loadFixture(collection, fixturePath) {
+  return function(done) {
+    var args = [
+      '--db', defaults.databaseName,
+      '--collection', collection
+    ];
+    var mongoimport = spawn('mongoimport', args);
+
+    fs.createReadStream(fixturePath).pipe(mongoimport.stdin);
+
+    mongoimport.stdout.on('data', function(data) {
+      console.log(data.toString());
+    });
+
+    mongoimport.stderr.on('data', function(data) {
+      console.error(data.toString());
+    });
+
+    mongoimport.on('close', function(code) {
+      done(null, code);
+    });
+  };
 }
