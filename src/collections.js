51ece1ba4110ff0bd4744fb8125bfc79f51f397d
Chris Mytton <self@hecticjeff.net>
2014-02-27 16:32:04 +0000

    Find memberships using the "member" property
    This retains backwards compatibility with the previous implementation
    that used `person_id` to identify the person that was associated with
    the membership. Now you can associate any type of object with a
    membership, though it's most likely to be a person or an organization.

--- src/collections.js
+++ src/collections.js
@@ -2,9 +2,12 @@
 
 function membershipFinder(schema, options) {
   schema.pre('init', function(next, data) {
+    var queries = [];
     var query = {};
     query[options.field] = data._id;
-    this.model('Membership').find(query, function(err, docs) {
+    queries.push(query);
+    queries.push({'member.@type': this.constructor.modelName, 'member.id': data._id});
+    this.model('Membership').find({$or: queries}, function(err, docs) {
       if (err) {
         return next(err);
       }
