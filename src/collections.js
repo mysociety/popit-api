dc1cecf9078d150800fea8339777b72bb9b50475
Chris Mytton <self@hecticjeff.net>
2014-03-04 17:03:14 +0000

    Move the membership finder plugin into a module
    For consistency with the rest of the codebase, mongoose plugins should
    live in "src/mongoose/".

--- src/collections.js
+++ src/collections.js
@@ -1,21 +1,6 @@
 "use strict";
 
-function membershipFinder(schema, options) {
-  schema.pre('init', function(next, data) {
-    var queries = [];
-    var query = {};
-    query[options.field] = data._id;
-    queries.push(query);
-    queries.push({'member.@type': this.constructor.modelName, 'member.id': data._id});
-    this.model('Membership').find({$or: queries}, function(err, docs) {
-      if (err) {
-        return next(err);
-      }
-      data.memberships = docs;
-      next();
-    });
-  });
-}
+var membershipFinder = require('./mongoose/membership-finder');
 
 /**
  * The various collections that the API will serve. This is intended to be
