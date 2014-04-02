8f3906e2aea9faa4302dac741ff3383ee79b1f2f
Chris Mytton <self@hecticjeff.net>
2014-04-02 14:25:34 +0100

    Refactor i18n middleware to use eachSchema function

--- src/middleware/i18n.js
+++ src/middleware/i18n.js
@@ -1,18 +1,14 @@
 "use strict";
 
-var collections = require('../collections');
+var eachSchema = require('../utils').eachSchema;
 
 function i18n(defaultLanguage) {
   defaultLanguage = defaultLanguage || 'en';
   return function i18nMiddleware(req, res, next) {
-    for (var key in collections) {
-      var schema = req.db.model(collections[key].model).schema;
-
+    eachSchema(req.db, function(schema) {
       schema.options.toJSON.langs = (req.accept && req.accept.languages);
-
       schema.options.toJSON.defaultLanguage = defaultLanguage;
-    }
-
+    });
     next();
   };
 }
