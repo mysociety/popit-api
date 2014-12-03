b89995ed86aab04260e13013d9d0b8174e43ae0b
Chris Mytton <self@hecticjeff.net>
2014-12-03 18:40:54 +0000

    Initial version of popolo json exporter
    Produces an object where the keys are the collection names and the
    values are arrays of objects for those collections.

---
+++ src/exporter.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var async = require('async');
+require('./models');
+
+/**
+ * Takes a mongoose connection and returns an object with all the collections
+ * to the callback.
+ *
+ * @param {Connection} connection A mongoose connection object
+ * @param {function} callback The function to call with the exported object
+ */
+function exporter(connection, callback) {
+  function exportCollection(name) {
+    return function(done) {
+      connection.model(name).find({}, {memberships: 0}, done);
+    };
+  }
+
+  async.parallel({
+    people: exportCollection('Person'),
+    organizations: exportCollection('Organization'),
+    memberships: exportCollection('Membership'),
+    posts: exportCollection('Post'),
+  }, callback);
+}
+
+module.exports = exporter;
