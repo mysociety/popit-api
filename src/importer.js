922604b0debe321b613b3d31382934b036f504b3
Chris Mytton <self@hecticjeff.net>
2014-12-05 18:26:45 +0000

    Populate _id fields before importing

--- src/importer.js
+++ src/importer.js
@@ -1,7 +1,23 @@
 "use strict";
 
 var async = require('async');
+var mongoose = require('mongoose');
 require('./models');
+
+function generateID() {
+  var objectId = new mongoose.Types.ObjectId();
+  return objectId.toHexString();
+}
+
+function populateIdField(record) {
+  if (record._id) {
+    return;
+  }
+  if (record.id) {
+    record._id = record.id;
+  }
+  record._id = generateID();
+}
 
 function importer(connection, popoloObject, callback) {
   var collections = [
@@ -17,6 +33,8 @@ function importer(connection, popoloObject, callback) {
     if (!popoloObject[key] || !Array.isArray(popoloObject[key])) {
       return done();
     }
+    // Ensure all records have a valid _id field
+    popoloObject[key].forEach(populateIdField);
     connection.model(modelName).create(popoloObject[key], done);
   }, callback);
 }
