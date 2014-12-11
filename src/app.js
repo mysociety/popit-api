059605c627b3cb7c9152d1c40743d98cdfafcd8a
Chris Mytton <self@hecticjeff.net>
2014-12-11 18:11:49 +0000

    Allow exporting in all languages

--- src/app.js
+++ src/app.js
@@ -79,12 +79,19 @@ function popitApiApp(options) {
     });
   });
 
+  // Export all languages - can potentially produce invalid popolo
   app.get('/export.json', function(req, res, next) {
     exporter(req.db, function(err, exportObject) {
       if (err) {
         return next(err);
       }
+      eachSchema(req.collection, function(schema) {
+        schema.options.toJSON.returnAllTranslations = true;
+      });
       res.send(exportObject);
+      eachSchema(req.collection, function(schema) {
+        schema.options.toJSON.returnAllTranslations = false;
+      });
     });
   });
 
@@ -98,6 +105,45 @@ function popitApiApp(options) {
         filename = options.instanceName + '-popolo-export.json.gz';
       } else {
         filename = 'popolo-export.json.gz';
+      }
+      res.attachment(filename);
+      var buf = new Buffer(JSON.stringify(exportObject), 'utf8');
+      zlib.gzip(buf, function(err, result) {
+        if (err) {
+          return next(err);
+        }
+        res.end(result);
+      });
+    });
+  });
+
+  // Export an individual language, this should be valid popolo
+  app.get('/export-:language.json', function(req, res, next) {
+    exporter(req.db, function(err, exportObject) {
+      if (err) {
+        return next(err);
+      }
+      eachSchema(req.collection, function(schema) {
+        schema.options.toJSON.langs = [req.params.language];
+      });
+      res.send(exportObject);
+    });
+  });
+
+  // Export an individual language, this should be valid popolo
+  app.get('/export-:language.json.gz', function(req, res, next) {
+    exporter(req.db, function(err, exportObject) {
+      if (err) {
+        return next(err);
+      }
+      eachSchema(req.collection, function(schema) {
+        schema.options.toJSON.langs = [req.params.language];
+      });
+      var filename;
+      if (options.instanceName) {
+        filename = options.instanceName + '-popolo-export-' + req.params.language + '.json.gz';
+      } else {
+        filename = 'popolo-export-' + req.params.language + '.json.gz';
       }
       res.attachment(filename);
       var buf = new Buffer(JSON.stringify(exportObject), 'utf8');
