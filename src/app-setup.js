d9c90351ca00265525470ca885114ccbce9044a7
Chris Mytton <chrismytton@gmail.com>
2015-02-18 11:41:47 +0000

    Increase request body size

--- src/app-setup.js
+++ src/app-setup.js
@@ -33,7 +33,7 @@ module.exports = function(app, options) {
     next();
   });
 
-  app.use(bodyParser.json());
+  app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(multer());
 
