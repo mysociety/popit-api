fd2e64819ddf5941e9be8db890e52bb98d21db18
Chris Mytton <chrism@mysociety.org>
2015-04-28 19:22:53 +0100

    Initial attempt to get instance working in the API

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 "use strict";
 
-var popitApi = require('./');
+var express = require('express');
+var app = express();
+var db = require('./db');
+
+var Instance = db.model('Instance');
+
+app.get('/instances', function(req, res, next) {
+  Instance.find(function(err, instances) {
+    if (err) {
+      return next(err);
+    }
+    res.send(instances);
+  });
+});
+
+app.param('instanceSlug', function(req, res, next, slug) {
+  Instance.findOne({slug: slug}, function(err, instance) {
+    if (err) {
+      return next(err);
+    }
+    if (!instance) {
+      return res.status(404).send({error: ["Could not find instance"]});
+    }
+    req.instance = instance;
+    next();
+  });
+});
+
+app.get('/instances/:instanceSlug', function(req, res, next) {
+  res.send(req.instance);
+});
+
+app.get('/instances/:instanceSlug/persons', function(req, res, next) {
+  var connection = db.connectionForInstance(req.instance);
+  var Person = connection.model('Person');
+  Person.find(function(err, people) {
+    if (err) {
+      return next(err);
+    }
+    res.send(people);
+  });
+});
 
 var port = process.env.PORT || 3000;
-
-var app = popitApi({storageSelector: 'hostName'});
-
 app.listen(port, function() {
   console.log("PopIt API listening on http://0.0.0.0:" + port);
 });
---
+++ db.js
@@ -0,0 +1,62 @@
+var mongoose = module.exports = require('mongoose');
+var config = require('config');
+var validator = require('validator');
+var format = require('util').format;
+
+var connectionStringFormat = config.get('mongodb.connectionStringFormat');
+var prefix = config.get('mongodb.prefix');
+var master = config.get('mongodb.master');
+
+// Main connection to master database
+mongoose.connect(format(connectionStringFormat, prefix + master));
+
+function connectionForInstance(instance) {
+  var dbName = instance.dbName || prefix + instance.slug;
+  return mongoose.createConnection(format(connectionStringFormat, dbName));
+}
+
+mongoose.connectionForInstance = connectionForInstance;
+
+// Need to account for popit_prefix length in maximum length of slug
+var maxSlugLength = 63 - config.get('mongodb.prefix').length;
+
+var InstanceSchema = mongoose.Schema({
+  slug: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    match: [new RegExp("^[a-z0-9][a-z0-9\-]{2," + (maxSlugLength-2) + "}[a-z0-9]$"), 'regexp'],
+    required: true,
+    unique: true,
+  },
+
+  created_date: {
+    type: Date,
+    'default': Date.now
+  },
+
+  dbname: {
+    type: String
+  },
+
+  status: {
+    type: String,
+    required: true,
+    'default': 'pending',
+    'enum': [
+      'pending',   // has not been activated yet - db does not exist
+      'active',    // created and available
+      'suspended', // blocked for some reason
+      'archived',  // has been archived due to lack of use
+    ],
+  },
+
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, 'not_an_email']
+  },
+
+});
+
+mongoose.model('Instance', InstanceSchema);
