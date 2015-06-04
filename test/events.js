c5f5d1507e2dd383eac670dd77a3a2ba4989b0e4
Chris Mytton <chrism@mysociety.org>
2015-06-04 09:40:30 +0100

    Add test for retrieving individual events

--- test/events.js
+++ test/events.js
@@ -4,10 +4,25 @@ var assert = require('assert');
 var supertest = require('supertest');
 var app = require('../');
 var defaults = require('./defaults');
+var mongoose = require('mongoose');
+var fixture = require("./fixture");
+
+require('../src/models');
 
 var request = supertest(app({databaseName: defaults.databaseName}));
 
 describe("Events API", function() {
+
+  beforeEach(fixture.clearDatabase);
+
+  before(function() {
+    mongoose.connect('mongodb://localhost/' + defaults.databaseName);
+  });
+
+  after(function(done) {
+    mongoose.connection.close(done);
+  });
+
 
   ['v0.1', 'v1.0.0-alpha'].forEach(function(version) {
     describe(version, function() {
@@ -26,6 +41,30 @@ describe("Events API", function() {
           assert.equal(res.body.result.name, "56th UK Parliament");
           done();
         });
+      });
+
+      describe("individual events", function() {
+        beforeEach(function(done) {
+          var Event = mongoose.model('Event');
+          Event.create({
+            _id: "parliament-41",
+            name: "41st Parliament",
+            start_date: "2011-06-02",
+            location: "House of Commons",
+            classification: "parliament"
+          }, done);
+        });
+
+        it("allows retrieving them", function(done) {
+          request.get('/' + version + '/events/parliament-41')
+          .expect(200)
+          .end(function(err, res) {
+            assert.ifError(err);
+            assert.equal(res.body.result.name, "41st Parliament");
+            done();
+          });
+        });
+
       });
 
     });
